0933983ed4d3e5d592fd543a1a730745b25151ed
Oggl1nator <serega23lan@Mail.ru>
2021-01-25 03:19:36 +0300

    123

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MenuComponent} from './core/menu/menu.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {RestService} from './service/rest.service';
+import {LoginService} from './service/login.service';
+import {LoggerService} from './service/logger.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+  ],
+  providers: [RestService, LoginService, LoggerService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
